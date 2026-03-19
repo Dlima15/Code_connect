2c3c7a2e08f57537d4d3e0db7c7c10d3cdb8be7d
Dlima15 <danilo.lima7456@gmail.com>
2026-03-18 23:54:18 -0300

    finalizando estilização do botão, e adicionando um evento para ele

---
+++ scripts.js
@@ -0,0 +1,8 @@
+console.log("Js aplicado")
+
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("image-upload");
+
+uploadBtn.addEventListener("click",() =>{
+    inputUpload.click();
+});
