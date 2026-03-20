c13d62b06b8ebd56853db0b5bba79847cbae6cc6
Dlima15 <danilo.lima7456@gmail.com>
2026-03-20 16:54:57 -0300

    criando a função de ler o conteudo

--- scripts.js
+++ scripts.js
@@ -6,3 +6,18 @@ const inputUpload = document.getElementById("image-upload");
 uploadBtn.addEventListener("click",() =>{
     inputUpload.click();
 });
+
+function LerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+            leitor.onload =() => {
+                resolve({url: leitor.result, nome: arquivo.name})
+            }
+
+            leitor.onerror =() =>{
+                reject(`Erro na leitura do arquivo ${arquivo.name}`)
+            }
+
+            leitor.readAsDataURL(arquivo)
+    })
+}
